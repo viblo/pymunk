16cd356c80f445acf226fd30b9b7cb7f57b09be7
google@viblo.se <google@viblo.se@78f3efd8-5644-0410-8aa8-f3833044bd4a>
2008-02-17 21:05:24 +0000

    Demo to draw contacts with arbiters in the end instead of in the collision callback

--- demo_contact.py
+++ demo_contact.py
@@ -12,17 +12,9 @@ COLLTYPE_DEFAULT = 0
 COLLTYPE_MOUSE = 1
 
 
-def flipy(y):
+def to_pygame(p):
     """Small hack to convert pymunk to pygame coordinates"""
-    return -y+600
-
-def coll_func(shapeA, shapeB, contacts, normal_coef, screen):
-    """Draw a circle at the contact, with larger radius for greater collisions"""
-    for c in contacts:
-        r = max( 3, abs(c.distance*5) )
-        p = c.position.x, flipy(c.position.y)
-        pygame.draw.circle(screen, THECOLORS["red"], p, r, 0)
-    return True
+    return int(p.x), int(-p.y+600)
 
 def main():
     
@@ -54,9 +46,6 @@ def main():
     
     ticks_to_next_ball = 10
 
-    ### Here we set the collision callback function
-    space.set_default_collisionpair_func(coll_func, screen)
-
     while running:
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -82,8 +71,8 @@ def main():
         for ball in balls:
             if ball.body.position.y < 200: balls_to_remove.append(ball)
 
-            p = ball.body.position.x, flipy(ball.body.position.y)
-            pygame.draw.circle(screen, THECOLORS["blue"], p, ball.radius, 2)
+            p = to_pygame(ball.body.position)
+            pygame.draw.circle(screen, THECOLORS["blue"], p, int(ball.radius), 2)
     
         for ball in balls_to_remove:
             space.remove(ball, ball.body)
@@ -93,12 +82,18 @@ def main():
             body = line.body
             pv1 = body.position + line.a.rotated(math.degrees(body.angle))
             pv2 = body.position + line.b.rotated(math.degrees(body.angle))
-            p1 = pv1.x, flipy(pv1.y)
-            p2 = pv2.x, flipy(pv2.y)
+            p1 = to_pygame(pv1)
+            p2 = to_pygame(pv2)
             pygame.draw.lines(screen, THECOLORS["lightgray"], False, [p1,p2])
-
+            
+        for arb in space.arbiters:
+            for c in arb.contacts:
+                r = max( 3, abs(c.distance*5) )
+                r = int(r)
+                p = to_pygame(c.position)
+                pygame.draw.circle(screen, THECOLORS["red"], p, r, 0)
+            
         ### Update physics
-        # (this will also draw the contacts)
         dt = 1.0/60.0
         for x in range(1):
             space.step(dt)
