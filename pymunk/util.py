7448e658a8e059ae734f3a4aa1b5129ab0367f97
google@viblo.se <google@viblo.se@78f3efd8-5644-0410-8aa8-f3833044bd4a>
2008-03-08 20:36:30 +0000

    Added calc_center function, better documentation

--- pymunk/util.py
+++ pymunk/util.py
@@ -1,7 +1,14 @@
+"""Contains utility functions, mainly to help with polygon creation"""
+__docformat__ = "reStructuredText"
+
 from functools import partial
 
 def is_clockwise(points): 
-    """Check if the points given forms a clockwise polygon"""
+    """
+    Check if the points given forms a clockwise polygon
+    
+    :return: True if the points forms a clockwise polygon
+    """
     a = 0
     i,j = 0,0
     for i in range(len(points)):
@@ -12,9 +19,10 @@ def is_clockwise(points):
     
 def is_left(p0, p1, p2):
     """Test if p2 is left, on or right of the (infinite) line (p0,p1).
-    > 0 for p2 left of the line through p0 and p1
-    = 0 for p2 on the line
-    < 0 for p2 right of the line
+    
+    :return: > 0 for p2 left of the line through p0 and p1
+        = 0 for p2 on the line
+        < 0 for p2 right of the line
     """
     # cast the answer to an int so it can be used directly from sort()
     # TODO: cast is not a good idea.. use something else
@@ -27,6 +35,8 @@ def is_left(p0, p1, p2):
 def reduce_poly(points, tolerance=500):
     """Remove close points to simplify a polyline
     tolerance is the min distance between two points squared.
+    
+    :return: The reduced polygon as a list of (x,y)
     """
     curr_p = points[0]
     reduced_ps = [points[0]]
@@ -41,6 +51,8 @@ def reduce_poly(points, tolerance=500):
 def convex_hull(points):
     """Create a convex hull from a list of points.
     This function uses the Graham Scan Algorithm.
+    
+    :return: Convex hull as a list of (x,y)
     """
     ### Find lowest rightmost point
     p0 = points[0]
@@ -75,4 +87,18 @@ def convex_hull(points):
                 l = is_left(pt2, pt1, p)
             hull.append(p)
     return hull 
+    
+def calc_center(points):
+    """Calculate the center of a polygon
+    
+    :return: The center (x,y)
+    """
+    tot_x, tot_y = 0,0
+    for p in points:
+        tot_x += p[0]
+        tot_y += p[1]
+    n = len(points)
+    return (tot_x/n, tot_y/n)
+    
+    
     
