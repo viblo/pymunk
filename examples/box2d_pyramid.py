a90fbaffea96aa4b303bbd3f49443a7590e2dc91
vb@viblo.se <vb@viblo.se@78f3efd8-5644-0410-8aa8-f3833044bd4a>
2008-04-27 18:41:41 +0000

    added two box2d demo remakes, box2d_pyramid.py and box2d_vertical_stack.py

---
+++ examples/box2d_pyramid.py
@@ -0,0 +1,124 @@
+"""
+Remake of the pyramid demo from the box2d testbed.
+"""
+
+import pygame
+from pygame.locals import *
+from pygame.color import *
+import pymunk
+from pymunk import Vec2d
+
+
+class PyramidDemo:
+    def flipyv(self, v):
+        v = Vec2d(v)
+        v.y = -v.y+self.h
+        return v
+        
+    def __init__(self):
+        self.running = True
+        self.drawing = True
+        self.w, self.h = 600,600
+        self.screen = pygame.display.set_mode((self.w, self.h))
+        self.clock = pygame.time.Clock()
+
+        ### Init pymunk and create space
+        pymunk.init_pymunk()
+        self.space = pymunk.Space()
+        self.space.gravity = Vec2d(0.0, -900.0)
+        
+        ### ground
+        body = pymunk.Body(pymunk.inf, pymunk.inf)
+        shape = pymunk.Segment(body, Vec2d(50, 100), Vec2d(550,100), .0)
+        shape.friction = 1.0
+        self.space.add_static(shape)
+        
+        ### pyramid
+        x=Vec2d(-100, 7.5) + (300,100)
+        y=Vec2d(0,0) 
+        deltaX=Vec2d(0.5625, 2.0)*10
+        deltaY=Vec2d(1.125, 0.0)*10
+
+        for i in range(25):
+            y = Vec2d(x)
+            for j in range(i, 25):
+                size= 5
+                points = map(Vec2d, [(-size, -size), (-size, size), (size,size), (size, -size)])
+                mass = 10.0
+                moment = pymunk.moment_for_poly(mass, points, Vec2d(0,0))
+                body = pymunk.Body(mass, moment)
+                body.position = y
+                shape = pymunk.Poly(body, points, Vec2d(0,0))
+                shape.friction = 1
+                self.space.add(body,shape)
+                
+                y += deltaY
+
+            x += deltaX
+            
+        self.space.resize_active_hash(dim=10, count=3250)
+    
+    def run(self):
+        while self.running:
+            self.loop() 
+            
+            
+    def loop(self):  
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                self.running = False
+            elif event.type == KEYDOWN and event.key == K_ESCAPE:
+                self.running = False
+            elif event.type == KEYDOWN and event.key == K_d:
+                self.drawing = not self.drawing
+        
+        steps = 3
+        dt = 1.0/120.0/steps            
+        for x in range(steps):
+            self.space.step(dt)
+        if self.drawing:
+            self.draw()
+        
+        ### Tick clock and update fps in title
+        self.clock.tick(30)
+        pygame.display.set_caption("fps: " + str(self.clock.get_fps()))
+        
+    def draw(self):
+        ### Clear the screen
+        self.screen.fill(THECOLORS["white"])
+        
+        for shape in self.space.static_shapes:
+            body = shape.body
+            pv1 = self.flipyv(body.position + shape.a.cpvrotate(body.rotation_vector))
+            pv2 = self.flipyv(body.position + shape.b.cpvrotate(body.rotation_vector))
+            pygame.draw.lines(self.screen, THECOLORS["lightgray"], False, [pv1,pv2])           
+    
+        for shape in self.space.shapes:
+            body = shape.body
+            ps = shape.get_points()
+            ps.append(ps[0])
+            ps = map(self.flipyv, ps)
+            color = THECOLORS["green"]
+            #pygame.draw.lines(self.screen, color, False, ps, 1)
+            pygame.draw.polygon(self.screen, THECOLORS["lightgray"], ps)
+            pygame.draw.polygon(self.screen, THECOLORS["darkgrey"], ps,1)
+
+        ### All done, lets flip the display
+        pygame.display.flip()        
+
+def main():
+    demo = PyramidDemo()
+    demo.run()
+
+if __name__ == '__main__':
+    doprof = 0
+    if not doprof: 
+        main()
+    else:
+        import cProfile, pstats
+        
+        prof = cProfile.run("main()", "profile.prof")
+        stats = pstats.Stats("profile.prof")
+        stats.strip_dirs()
+        stats.sort_stats('cumulative', 'time', 'calls')
+        stats.print_stats(30)
---
+++ examples/box2d_vertical_stack.py
@@ -0,0 +1,128 @@
+"""
+Remake of the veritcal stack demo from the box2d testbed.
+"""
+
+import math
+
+import pyglet
+from pyglet.gl import *
+from pyglet.window import key, mouse
+
+import pymunk
+from pymunk import Vec2d
+
+class Main(pyglet.window.Window):
+    def __init__(self):
+        
+        pyglet.window.Window.__init__(self)
+        self.set_caption('Vertical stack from box2d')
+        pymunk.init_pymunk()
+        
+        pyglet.clock.schedule_interval(self.update, 1/60.0)
+        self.fps_display = pyglet.clock.ClockDisplay()
+        
+        self.text = pyglet.text.Label('Press space to fire bullet',
+                          font_name='TArial',
+                          font_size=10,
+                          x=10, y=400)
+        self.create_world()
+        
+    
+
+    def create_world(self):
+        self.space = pymunk.Space()
+        self.space.gravity = Vec2d(0.,-900.)
+        
+        static_body = pymunk.Body(pymunk.inf, pymunk.inf)
+        static_lines = [pymunk.Segment(static_body, Vec2d(50,100), Vec2d(550,100), 1),
+                        pymunk.Segment(static_body, Vec2d(450,100), Vec2d(450,300), 1)
+                        ]
+        for l in static_lines:
+            l.friction = 0.3
+        self.space.add_static(static_lines)
+        
+        for x in range(5):
+            for y in range(12):
+                size= 5
+                points = map(Vec2d, [(-size, -size), (-size, size), (size,size), (size, -size)])
+                mass = 10.0
+                moment = pymunk.moment_for_poly(mass, points, Vec2d(0,0))
+                body = pymunk.Body(mass, moment)
+                body.position = Vec2d(200 + x*50, 105 + y * 11)
+                shape = pymunk.Poly(body, points, Vec2d(0,0))
+                shape.friction = 0.3
+                self.space.add(body,shape)
+                
+        
+    def update(self,dt):
+        steps = 10
+        for x in range(steps):
+            self.space.step(1/60./steps)
+
+
+    def on_key_press(self, symbol, modifiers):
+        if symbol == key.SPACE:
+            mass = 100
+            r = 5
+            moment = pymunk.moment_for_circle(mass, 0, r, Vec2d(0,0))
+            body = pymunk.Body(mass, moment)
+            body.position = Vec2d(0, 165)
+            shape = pymunk.Circle(body, r, Vec2d(0,0))
+            self.space.add(body, shape)
+            f = 200000
+            body.apply_impulse(Vec2d(f,0), Vec2d(0,0))
+        elif symbol == key.ESCAPE:
+            pyglet.app.exit()
+            
+
+    def on_draw(self):
+        self.clear()
+        self.text.draw()
+        self.fps_display.draw()
+
+        for shape in self.space.static_shapes:
+            if isinstance(shape, pymunk.Segment):
+                body = shape.body
+                pv1 = body.position + shape.a.rotated(math.degrees(body.angle))
+                pv2 = body.position + shape.b.rotated(math.degrees(body.angle))
+                pyglet.graphics.draw(2, GL_LINES,
+                                     ('v2f', (pv1.x, pv1.y, pv2.x, pv2.y)),
+                                      ('c4f', (1.0, 1.0, 1.0, 1.) * 2))
+        
+        b = pyglet.graphics.Batch()
+        
+        for shape in self.space.shapes:
+            if isinstance(shape, pymunk.Poly):
+                ps = shape.get_points()
+                ps = [ps[0]] + ps + [ps[0], ps[0]]
+                xs = []
+                for p in ps:
+                    xs.append(p.x)
+                    xs.append(p.y)
+                b.add(len(ps), GL_LINE_STRIP, None, 
+                        ('v2f', xs),
+                         ('c4f', (0.0, 1.0, 0.0, 1.0) * len(ps)))
+                #pyglet.graphics.draw(len(ps), GL_LINE_STRIP,
+                #                     ('v2f', xs),
+                #                      ('c4f', (0.0, 1.0, 0.0, 1.0) * len(ps)))
+                                      
+            if isinstance(shape, pymunk.Circle):
+                p = shape.body.position
+                ps = [p + (0,shape.radius), p + (shape.radius,0),
+                        p + (0,-shape.radius), p + (-shape.radius,0)]
+                ps += [ps[0]]
+                xs = []
+                for p in ps:
+                    xs.append(p.x)
+                    xs.append(p.y)
+                pyglet.graphics.draw(len(ps), GL_LINE_STRIP,
+                                     ('v2f', xs),
+                                      ('c4f', (0.0, 0.0, 1.0, 1.0) * len(ps)))
+        
+        b.draw()
+if __name__ == '__main__':
+    doprof = 0
+    
+    main = Main()
+    if not doprof: 
+        pyglet.app.run()
