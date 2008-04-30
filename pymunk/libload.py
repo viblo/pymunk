839a6ef5946ff3974d38fe6e76f4932c320e590b
vb@viblo.se <vb@viblo.se@78f3efd8-5644-0410-8aa8-f3833044bd4a>
2008-04-30 15:17:31 +0000

    new chipmunk dll, modified libload to load libchipmunk.so in the case it doesn't find 32/64

--- pymunk/libload.py
+++ pymunk/libload.py
@@ -18,18 +18,28 @@ def load_library(libname, print_path=True):
             path = os.path.dirname(os.path.abspath(sys.executable))
     except:
         pass
+
     if s == 'Linux':
         libfn = "%s%s.so" % (libname, arch[:2])
+        libfn_default = "%s.so" % libname
         
     elif s == 'Windows':
         libfn = "%s.dll" % libname
-        
+        libfn_default=libfn
     elif s == 'Darwin':
         libfn = "%s.dylib" % libname
- 
+        libfn_default=libfn
+        
     libfn = os.path.join(path, libfn)
-    if print_path:
-        print "Loading chipmunk for %s (%s) [%s]" % (s, arch, libfn)
-    lib = ctypes.cdll.LoadLibrary(libfn)
- 
+    libfn_default = os.path.join(path, libfn_default)
+    try:
+        if print_path:
+            print "Loading chipmunk for %s (%s) [%s]" % (s, arch, libfn)
+        lib = ctypes.cdll.LoadLibrary(libfn)
+    except:
+        if libfn == libfn_default: 
+            raise
+        if print_path:
+            print "Loading chipmunk for %s (%s) [%s]" % (s, arch, libfn)
+        lib = ctypes.cdll.LoadLibrary(libfn)
     return lib
