c0ea28a8479bbf29be8f91c53eb1d1967fb4539e
vb@viblo.se <vb@viblo.se@78f3efd8-5644-0410-8aa8-f3833044bd4a>
2008-11-01 23:48:04 +0000

    small changes to craete_release-script

--- tools/create_release_win32.py
+++ tools/create_release_win32.py
@@ -7,7 +7,8 @@ def main():
     os.chdir("..")
     os.system("python setup.py sdist")
     os.system("python setup.py bdist --formats=wininst")
-    print("""remember to (before running this script!): 
+    print("""
+    Remember (before running this script!): 
     - change version number in readme and __init__ 
     - regenerate the api
     - branch in svn""")
