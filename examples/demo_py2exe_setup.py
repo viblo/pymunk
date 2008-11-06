8dc7f0c5dc0db6b3880471d162daa3bf130bddc1
vb@viblo.se <vb@viblo.se@78f3efd8-5644-0410-8aa8-f3833044bd4a>
2008-11-06 21:00:48 +0000

    Added example file for how to make pymunk scripts work with py2exe.

---
+++ examples/demo_py2exe_setup.py
@@ -0,0 +1,12 @@
+import os
+from distutils.core import setup
+import py2exe
+import pymunk
+
+pymunk_dir = os.path.dirname(pymunk.__file__)
+
+setup(console=['demo_basic_test.py'],
+    data_files = [os.path.join(pymunk_dir, 'chipmunk.dll')]
+    )
+    
+
