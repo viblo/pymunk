2224512ee68a44fac7bd91644d7cdc69b884cbe0
vb@viblo.se <vb@viblo.se@78f3efd8-5644-0410-8aa8-f3833044bd4a>
2012-06-02 23:00:54 +0000

    added a small run.py script to help out to run the scripts in examples folder in case you have the whole source tree of pymunk and dont want to install it or put the pymunk folder inside the examples folder.

---
+++ examples/run.py
@@ -0,0 +1,19 @@
+desc = """
+Use to run examples using pymunk located one folder level up.
+Useful if you have the whole pymunk source tree and want to run the examples 
+in a quick and dirty way.
+
+Example:
+>python run.py breakout.py
+"""
+
+import sys
+sys.path.insert(0,'..')
+
+if len(sys.argv) > 1:
+    name = sys.argv[1]
+    sys.argv = sys.argv[1:]
+    
+    execfile(name)
+else:
+    print desc
