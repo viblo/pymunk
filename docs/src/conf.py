010cf53f84da112b50a3d2f74f6132b83e36826f
vb@viblo.se <vb@viblo.se@78f3efd8-5644-0410-8aa8-f3833044bd4a>
2012-10-29 20:33:48 +0000

    modified sphinx conf to always include __init__ methods in docs.

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -312,3 +312,12 @@ texinfo_documents = [
 
 # How to display URL addresses: 'footnote', 'no', or 'inline'.
 #texinfo_show_urls = 'footnote'
+
+# Code to make sphinx always include the __init__ method in autodocs
+def skip(app, what, name, obj, skip, options):
+    if name == "__init__":
+        return False
+    return skip
+
+def setup(app):
+    app.connect("autodoc-skip-member", skip)
