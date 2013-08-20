c2d2f6068250f21dbc85758fe7c2b69ca469ac6f
vb@viblo.se <vb@viblo.se@78f3efd8-5644-0410-8aa8-f3833044bd4a>
2013-08-20 17:06:52 +0000

    regen of _chipmunk

--- pymunk/_chipmunk.py
+++ pymunk/_chipmunk.py
@@ -21,11 +21,10 @@ free = None # symbol removed
 free = None # symbol removed
 free = None # symbol removed
 cpfree = free # alias
-cpArbiterStateIgnore = 2
-cpArbiterStateCached = 3
-cpArbiterStateNormal = 1
+cpArbiterStateFirstColl = 0
 # cpfpow = pow # alias
 # cpfmod = fmod # alias
+# cpffloor = floor # alias
 # cpfexp = exp # alias
 # cpfcos = cos # alias
 # cpfceil = ceil # alias
@@ -38,17 +37,18 @@ calloc = None # symbol removed
 cpcalloc = calloc # alias
 # def cpAssertSoft(__condition__,...): return if(!(__condition__)) cpMessage(#__condition__, __FILE__, __LINE__, 1, 0, __VA_ARGS__) # macro
 # def cpAssertHard(__condition__,...): return if(!(__condition__)) cpMessage(#__condition__, __FILE__, __LINE__, 1, 1, __VA_ARGS__) # macro
-# def cpConstraintCheckCast(constraint,struct): return cpAssertHard(constraint->CP_PRIVATE(klass) == struct ##GetClass(), "Constraint is not a "#struct) # macro
 def cpBodyAssertSane(body): return cpBodySanityCheck(body) # macro
 # def cpAssertWarn(__condition__,...): return if(!(__condition__)) cpMessage(#__condition__, __FILE__, __LINE__, 0, 0, __VA_ARGS__) # macro
-# cpffloor = floor # alias
-cpArbiterStateFirstColl = 0
 # cpfsqrt = sqrt # alias
-# cpfsin = sin # alias
+cpArbiterStateIgnore = 2
+cpArbiterStateCached = 3
 realloc = None # symbol removed
 realloc = None # symbol removed
 realloc = None # symbol removed
 cprealloc = realloc # alias
+cpArbiterStateNormal = 1
+# def cpConstraintCheckCast(constraint,struct): return cpAssertHard(constraint->CP_PRIVATE(klass) == struct ##GetClass(), "Constraint is not a "#struct) # macro
+# cpfsin = sin # alias
 cpMessage = chipmunk_lib.cpMessage
 cpMessage.restype = None
 cpMessage.argtypes = [STRING, STRING, c_int, c_int, c_int, STRING]
