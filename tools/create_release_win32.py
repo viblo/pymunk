dbafba5a7590ea2842035073e7cd767a9c42bec2
vb@viblo.se <vb@viblo.se@78f3efd8-5644-0410-8aa8-f3833044bd4a>
2013-08-21 17:23:53 +0000

    small improvement to create release script

--- tools/create_release_win32.py
+++ tools/create_release_win32.py
@@ -3,12 +3,14 @@ import os
 
 
 def main():
-    
+    upload_command = ""
+    if len(sys.argv) > 1 and sys.argv[1] == "upload":
+        upload_command = " upload"
     os.chdir("..")
-    os.system("python setup.py sdist")
+    os.system("python setup.py sdist" + upload_command)
     os.system("python setup.py bdist --formats=msi")
     os.system("python25 setup.py bdist --formats=msi")
-    os.system("python setup.py bdist --formats=wininst")
+    os.system("python setup.py bdist --formats=wininst" + upload_command)
     print("""
     Remember (before running this script!): 
     - change version number in readme, setup.py and __init__ 
@@ -16,7 +18,9 @@ def main():
     - make sure chipmunk.dll does not depend on msvcr90.dll!
       (best way is to compile with python 2.5 instead of 2.6)
     - regenerate the api
-    - branch in svn""")
+    - branch in svn
+    - run this script with >python create_release_win32.py upload to upload to 
+    pypy""")
     
 
 
