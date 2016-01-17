56b167b6d9e1279074f6902e672e19302314e26e
Victor <vb@viblo.se>
2016-01-17 20:26:18 +0800

    refactor tests

--- tests/test_common.py
+++ tests/test_common.py
@@ -34,53 +34,6 @@ class UnitTestGeneral(unittest.TestCase):
         a = p.area_for_poly([(0,0), (10,10), (10,0)], 3)
         self.assertAlmostEqual(a, 80.700740753)
 
-class UnitTestShapeFilter(unittest.TestCase):
-    def testInit(self):
-        f = p.ShapeFilter()
-        self.assertEqual(f.group, 0)
-        self.assertEqual(f.categories, 0xffffffff)
-        self.assertEqual(f.mask, 0xffffffff)
-
-        f = p.ShapeFilter(1,2,3)
-        self.assertEqual(f.group, 1)
-        self.assertEqual(f.categories, 2)
-        self.assertEqual(f.mask, 3)
-
-    def testEq(self):
-        f1 = p.ShapeFilter(1,2,3)
-        f2 = p.ShapeFilter(1,2,3)
-        f3 = p.ShapeFilter(2,3,4)
-        self.assertTrue(f1 == f2)
-        self.assertTrue(f1 != f3)
-
-class UnitTestTransform(unittest.TestCase):
-    def testInit(self):
-        t = p.Transform()
-        self.assertEqual(t.a, 0)
-        self.assertEqual(t.b, 0)
-        self.assertEqual(t.c, 0)
-        self.assertEqual(t.d, 0)
-        self.assertEqual(t.tx, 0)
-        self.assertEqual(t.ty, 0)
-
-        t = p.Transform(1,2,3,4,5,6)
-        self.assertEqual(t.a, 1)
-        self.assertEqual(t.b, 2)
-        self.assertEqual(t.c, 3)
-        self.assertEqual(t.d, 4)
-        self.assertEqual(t.tx, 5)
-        self.assertEqual(t.ty, 6)
-        self.assertEqual(str(t), "Transform(1.0,2.0,3.0,4.0,5.0,6.0)")
-
-    def testIdentity(self):
-        t = p.Transform.identity()
-        self.assertEqual(t.a, 1)
-        self.assertEqual(t.b, 0)
-        self.assertEqual(t.c, 0)
-        self.assertEqual(t.d, 1)
-        self.assertEqual(t.tx, 0)
-        self.assertEqual(t.ty, 0)
-
 class UnitTestBugs(unittest.TestCase):
     def testManyBoxCrash(self):
         space = p.Space()
