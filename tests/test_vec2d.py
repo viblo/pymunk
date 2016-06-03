7c34688b7308715e4ceaa17cfb76513b0b96714f
Victor <vb@viblo.se>
2016-06-03 09:30:05 +0800

    replace some deprecated asserts

--- tests/test_vec2d.py
+++ tests/test_vec2d.py
@@ -64,7 +64,7 @@ class UnitTestVec2d(unittest.TestCase):
         self.assert_(v.length == 5)
         self.assert_(v.get_length_sqrd() == 25)
         self.assert_(v.normalize_return_length() == 5)
-        self.assertAlmostEquals(v.length, 1)
+        self.assertAlmostEqual(v.length, 1)
         v.length = 5
         self.assert_(v == Vec2d(3, 4))
         v2 = Vec2d(10, -2)
@@ -72,65 +72,65 @@ class UnitTestVec2d(unittest.TestCase):
         
     def testAnglesDegrees(self):            
         v = Vec2d(0, 3)
-        self.assertEquals(v.angle_degrees, 90)
+        self.assertEqual(v.angle_degrees, 90)
         v2 = Vec2d(v)
         v.rotate_degrees(-90)
         self.assertEqual(v.get_angle_degrees_between(v2), 90)
         v2.angle_degrees -= 90
         self.assertEqual(v.length, v2.length)
-        self.assertEquals(v2.angle_degrees, 0)
+        self.assertEqual(v2.angle_degrees, 0)
         self.assertEqual(v2, [3, 0])
         self.assert_((v - v2).length < .00001)
         self.assertEqual(v.length, v2.length)
         v2.rotate_degrees(300)
-        self.assertAlmostEquals(v.get_angle_degrees_between(v2), -60) # Allow a little more error than usual (floats..)
+        self.assertAlmostEqual(v.get_angle_degrees_between(v2), -60) # Allow a little more error than usual (floats..)
         v2.rotate_degrees(v2.get_angle_degrees_between(v))
         angle = v.get_angle_degrees_between(v2)
-        self.assertAlmostEquals(v.get_angle_degrees_between(v2), 0)  
+        self.assertAlmostEqual(v.get_angle_degrees_between(v2), 0)  
 
     def testAnglesRadians(self):            
         v = Vec2d(0, 3)
-        self.assertEquals(v.angle, math.pi/2.)
+        self.assertEqual(v.angle, math.pi/2.)
         v2 = Vec2d(v)
         v.rotate(-math.pi/2.)
         self.assertEqual(v.get_angle_between(v2), math.pi/2.)
         v2.angle -= math.pi/2.
         self.assertEqual(v.length, v2.length)
-        self.assertEquals(v2.angle, 0)
+        self.assertEqual(v2.angle, 0)
         self.assertEqual(v2, [3, 0])
-        self.assert_((v - v2).length < .00001)
+        self.assertTrue((v - v2).length < .00001)
         self.assertEqual(v.length, v2.length)
         v2.rotate(math.pi/3.*5.)
-        self.assertAlmostEquals(v.get_angle_between(v2), -math.pi/3.) # Allow a little more error than usual (floats..)
+        self.assertAlmostEqual(v.get_angle_between(v2), -math.pi/3.) # Allow a little more error than usual (floats..)
         v2.rotate(v2.get_angle_between(v))
         angle = v.get_angle_between(v2)
-        self.assertAlmostEquals(v.get_angle_between(v2), 0) 
+        self.assertAlmostEqual(v.get_angle_between(v2), 0) 
 
     def testHighLevel(self):
         basis0 = Vec2d(5.0, 0)
         basis1 = Vec2d(0, .5)
         v = Vec2d(10, 1)
-        self.assert_(v.convert_to_basis(basis0, basis1) == [2, 2])
-        self.assert_(v.projection(basis0) == (10, 0))
-        self.assert_(basis0.dot(basis1) == 0)
+        self.assertTrue(v.convert_to_basis(basis0, basis1) == [2, 2])
+        self.assertTrue(v.projection(basis0) == (10, 0))
+        self.assertTrue(basis0.dot(basis1) == 0)
         
     def testCross(self):
         lhs = Vec2d(1, .5)
         rhs = Vec2d(4, 6)
-        self.assert_(lhs.cross(rhs) == 4)
+        self.assertTrue(lhs.cross(rhs) == 4)
         
     def testComparison(self):
         int_vec = Vec2d(3, -2)
         flt_vec = Vec2d(3.0, -2.0)
         zero_vec = Vec2d(0, 0)
-        self.assert_(int_vec == flt_vec)
-        self.assert_(int_vec != zero_vec)
-        self.assert_((flt_vec == zero_vec) == False)
-        self.assert_((flt_vec != int_vec) == False)
-        self.assert_(int_vec == (3, -2))
-        self.assert_(int_vec != [0, 0])
-        self.assert_(int_vec != 5)
-        self.assert_(int_vec != [3, -2, -5])
+        self.assertTrue(int_vec == flt_vec)
+        self.assertTrue(int_vec != zero_vec)
+        self.assertTrue((flt_vec == zero_vec) == False)
+        self.assertTrue((flt_vec != int_vec) == False)
+        self.assertTrue(int_vec == (3, -2))
+        self.assertTrue(int_vec != [0, 0])
+        self.assertTrue(int_vec != 5)
+        self.assertTrue(int_vec != [3, -2, -5])
     
     def testInplace(self):
         inplace_vec = Vec2d(5, 13)
@@ -141,14 +141,14 @@ class UnitTestVec2d(unittest.TestCase):
         inplace_vec /= (3, 6)
         inplace_vec += Vec2d(-1, -1)
         alternate = (inplace_src*.5 + .5)/Vec2d(3, 6) + [-1, -1]
-        self.assertEquals(inplace_vec, inplace_ref)
-        self.assertEquals(inplace_vec, alternate)
+        self.assertEqual(inplace_vec, inplace_ref)
+        self.assertEqual(inplace_vec, alternate)
     
     def testPickle(self):
         testvec = Vec2d(5, .3)
         testvec_str = pickle.dumps(testvec)
         loaded_vec = pickle.loads(testvec_str)
-        self.assertEquals(testvec, loaded_vec)
+        self.assertEqual(testvec, loaded_vec)
 
 if __name__ == "__main__":
     print ("testing pymunk.vec2d version " + p.version)
