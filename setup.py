5614914a60bf8972467855d821c1f97daf7a1a49
Victor <vb@viblo.se>
2016-06-12 23:06:51 +0800

    tox working?

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
-from distutils.command.build_clib import build_clib
+#from distutils.command.build_clib import build_clib
+from distutils.command.build_ext import build_ext
+from setuptools import Extension
 import distutils.ccompiler as cc
 import os, os.path
 import platform
@@ -26,14 +28,14 @@ def get_library_name():
         libname += ".so"
     return libname
     
-class build_chipmunk_clib(build_clib, object):
+class build_chipmunk(build_ext, object):
 
     def finalize_options(self):
         if platform.system() == 'Windows':
             print("Running on Windows. GCC will be forced used")
             self.compiler = "mingw32"
         
-        return super(build_chipmunk_clib, self).finalize_options()
+        return super(build_chipmunk, self).finalize_options()
 
     def get_outputs(self):
         x = super(build_chipmunk, self).get_outputs()
@@ -43,100 +45,105 @@ class build_chipmunk_clib(build_clib, object):
         return self.xoutputs
         #return x
 
+    def run(self):
 
-    def build_libraries(self, libraries):
-        for (lib_name, build_info) in libraries:
-            compiler_preargs = ['-std=gnu99', 
-                                '-ffast-math', 
-                                '-DCHIPMUNK_FFI', 
-                                '-g',
-                                #'-Wno-unknown-pragmas', 
-                                #'-fPIC', 
-                                '-DCP_USE_CGPOINTS=0'] # '-DCP_ALLOW_PRIVATE_ACCESS']
-            if not self.debug:
-                compiler_preargs.append('-DNDEBUG')
+        self.compiler = cc.new_compiler(compiler=self.compiler)
+        
+        compiler_preargs = ['-std=gnu99', 
+                            '-ffast-math', 
+                            '-DCHIPMUNK_FFI', 
+                            '-g',
+                            #'-Wno-unknown-pragmas', 
+                            #'-fPIC', 
+                            '-DCP_USE_CGPOINTS=0'] # '-DCP_ALLOW_PRIVATE_ACCESS']
+        if not self.debug:
+            compiler_preargs.append('-DNDEBUG')
+        
+        if get_arch() == 64 and platform.system() == 'Linux':
+            compiler_preargs += ['-m64', '-fPIC', '-O3']
+        elif get_arch() == 32 and platform.system() == 'Linux':
+            compiler_preargs += ['-m32', '-fPIC', '-O3']
+        elif platform.system() == 'Darwin':
+            #No -O3 on OSX. There's a bug in the clang compiler when using O3.
+            compiler_preargs += ['-arch', 'i386', '-arch', 'x86_64']
+        
+        if platform.system() == 'Windows':
+            # Compile with stddecl instead of cdecl (-mrtd). 
+            # Using cdecl cause a missing bytes issue in some cases
+            # Because -mrtd and -fomit-frame-pointer (which is included in -O)
+            # gives problem with function pointer to the sdtlib free function
+            # we also have to use -fno-omit-frame-pointer
+            compiler_preargs += [#'-mrtd', 
+                                #'-O1',
+                                '-shared']
+                                #'-fno-omit-frame-pointer'] 
+            #O1 and O2 works on 32bit, not O3 and maybe not O0?
             
-            if get_arch() == 64 and platform.system() == 'Linux':
-                compiler_preargs += ['-m64', '-fPIC', '-O3']
-            elif get_arch() == 32 and platform.system() == 'Linux':
-                compiler_preargs += ['-m32', '-fPIC', '-O3']
-            elif platform.system() == 'Darwin':
-                #No -O3 on OSX. There's a bug in the clang compiler when using O3.
-                compiler_preargs += ['-arch', 'i386', '-arch', 'x86_64']
+            if get_arch() == 32:
+                # We set the stack boundary with -mincoming-stack-boundary=2
+                # from 
+                # https://mingwpy.github.io/issues.html#choice-of-msvc-runtime 
+                compiler_preargs += ['-O3', 
+                                    '-mincoming-stack-boundary=2',
+                                    '-m32']
+            if get_arch() == 64:
+                compiler_preargs += ['-O3', '-m64']
             
-            if platform.system() == 'Windows':
-                # Compile with stddecl instead of cdecl (-mrtd). 
-                # Using cdecl cause a missing bytes issue in some cases
-                # Because -mrtd and -fomit-frame-pointer (which is included in -O)
-                # gives problem with function pointer to the sdtlib free function
-                # we also have to use -fno-omit-frame-pointer
-                compiler_preargs += [#'-mrtd', 
-                                    #'-O1',
-                                    '-shared']
-                                    #'-fno-omit-frame-pointer'] 
-                #O1 and O2 works on 32bit, not O3 and maybe not O0?
+        for x in self.compiler.executables:
+            args = getattr(self.compiler, x)
+            try:
+                args.remove('-mno-cygwin') #Not available on newer versions of gcc 
+                args.remove('-mdll')
+            except:
+                pass
                 
-                if get_arch() == 32:
-                    # We set the stack boundary with -mincoming-stack-boundary=2
-                    # from 
-                    # https://mingwpy.github.io/issues.html#choice-of-msvc-runtime 
-                    compiler_preargs += ['-O3', 
-                                        '-mincoming-stack-boundary=2',
-                                        '-m32']
-                if get_arch() == 64:
-                    compiler_preargs += ['-O3', '-m64']
-                
-            for x in self.compiler.executables:
-                args = getattr(self.compiler, x)
-                try:
-                    args.remove('-mno-cygwin') #Not available on newer versions of gcc 
-                    args.remove('-mdll')
-                except:
-                    pass
+        source_folders = [os.path.join('chipmunk_src','src')]
+        sources = []
+        for folder in source_folders:
+            for fn in os.listdir(folder):
+                fn_path = os.path.join(folder, fn)
+                if fn_path[-1] == 'c':
+                    sources.append(fn_path)
+                elif fn_path[-1] == 'o':
+                    os.remove(fn_path)
+        
+        include_dirs = [os.path.join('chipmunk_src','include')]
+        
+        objs = self.compiler.compile(sources, 
+            include_dirs=include_dirs, extra_preargs=compiler_preargs)
             
-            sources = build_info.get('sources')
-            include_dirs = build_info.get('include_dirs')
-            
-            objs = self.compiler.compile(sources, 
-                include_dirs=include_dirs, extra_preargs=compiler_preargs)
-                
-            if platform.system() == 'Darwin':
-                self.compiler.set_executable('linker_so', 
-                    ['cc', '-dynamiclib', '-arch', 'i386', '-arch', 'x86_64'])
-            linker_preargs = []
-            if platform.system() == 'Linux' and platform.machine() == 'x86_64':
-                linker_preargs += ['-fPIC']
-            if platform.system() == 'Windows':
-                # link with stddecl instead of cdecl
-                #linker_preargs += ['-mrtd'] 
-                if get_arch() == 32:
-                    linker_preargs += ['-m32']
-                else:
-                    linker_preargs += ['-m64']
-                # remove link against msvcr*. this is a bit ugly maybe.. :)
-                self.compiler.dll_libraries = [lib for lib in self.compiler.dll_libraries if not lib.startswith("msvcr")]
-            here = os.path.abspath(os.path.dirname(__file__))
-            print here
-            
-            self.inplace = False
-            print("self.inplace", self.inplace)
-            
-            if not self.inplace:
-                package_dir = os.path.join(self.build_clib, "pymunk")
+        if platform.system() == 'Darwin':
+            self.compiler.set_executable('linker_so', 
+                ['cc', '-dynamiclib', '-arch', 'i386', '-arch', 'x86_64'])
+        linker_preargs = []
+        if platform.system() == 'Linux' and platform.machine() == 'x86_64':
+            linker_preargs += ['-fPIC']
+        if platform.system() == 'Windows':
+            # link with stddecl instead of cdecl
+            #linker_preargs += ['-mrtd'] 
+            if get_arch() == 32:
+                linker_preargs += ['-m32']
             else:
-                build_py = self.get_finalized_command('build_py')
-                package_dir = os.path.abspath(build_py.get_package_dir(".pymunk"))
-            self.xoutputs = [os.path.join(package_dir, get_library_name())]
-            #package_dir = self.build_lib
-            print("package_dir", package_dir)
-            #outpath = os.path.join(package_dir, get_library_name()) 
-
-            print("build_clib", self.build_clib)
-            
-            self.compiler.link(
-                cc.CCompiler.SHARED_LIBRARY, 
-                objs, get_library_name(),
-                output_dir = package_dir, extra_preargs=linker_preargs)    
+                linker_preargs += ['-m64']
+            # remove link against msvcr*. this is a bit ugly maybe.. :)
+            self.compiler.dll_libraries = [lib for lib in self.compiler.dll_libraries if not lib.startswith("msvcr")]
+        here = os.path.abspath(os.path.dirname(__file__))
+        print("here", here)
+        
+        print("self.inplace", self.inplace)
+        if not self.inplace:
+            package_dir = os.path.join(self.build_lib, "pymunk")
+        else:
+            build_py = self.get_finalized_command('build_py')
+            package_dir = os.path.abspath(build_py.get_package_dir(".pymunk"))
+        self.xoutputs = [os.path.join(package_dir, get_library_name())]
+        #package_dir = self.build_lib
+        print("package_dir", package_dir)
+        #outpath = os.path.join(package_dir, get_library_name()) 
+        self.compiler.link(
+            cc.CCompiler.SHARED_LIBRARY, 
+            objs, get_library_name(),
+            output_dir = package_dir, extra_preargs=linker_preargs)    
                 
                         
 # todo: add/remove/think about this list
@@ -171,10 +178,12 @@ for folder in source_folders:
         elif fn_path[-1] == 'o':
             os.remove(fn_path)
             
-libraries = [("chipmunk", {
+extensions = [("pymunk.chipmunk", {
     'sources': sources,
     'include_dirs': [os.path.join('chipmunk_src','include')]
 })]
+
+extensions = [Extension("pymunk.chipmunk", sources)]
 
 setup(
     name = 'pymunk',
@@ -186,17 +195,17 @@ setup(
     long_description = long_description,
     packages = ['pymunk','pymunkoptions'],
     
-    #, package_data = {'pymunk': ['chipmunk.dll'
+    #package_data = {'pymunk': ['chipmunk.dll'
     #                            , 'chipmunk64.dll'
     #                            , 'libchipmunk.so'
     #                            , 'libchipmunk64.so'
-    #                            , 'libchipmunk.dylib']}
+    #                            , 'libchipmunk.dylib']},
     include_package_data = True,
     license = 'MIT License',
     classifiers = classifiers,
-    cmdclass = {'build_clib':build_chipmunk_clib},
+    cmdclass = {'build_ext': build_chipmunk},
     install_requires = ['cffi'],
     extras_require = {'dev': ['pyglet','pygame','sphinx']},    
     test_suite = "tests",
-    libraries = libraries,
+    ext_modules = extensions,
 )
