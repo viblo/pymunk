eafee0f9156b5f1305e9e16e6acb7a59ec7ddc9e
Victor <vb@viblo.se>
2018-10-25 22:02:41 +0200

    run doctests from tox

--- tests/doctests.py
+++ tests/doctests.py
@@ -12,3 +12,10 @@ def load_tests(loader, tests, ignore):
             print("Skipping " + modname)
     tests.addTests(doctest.DocTestSuite(pymunk))
     return tests
+
+
+if __name__ == '__main__':
+    print("running doctests")
+    suite = unittest.TestSuite()  
+    load_tests(None, suite, None)
+    unittest.TextTestRunner().run(suite)
