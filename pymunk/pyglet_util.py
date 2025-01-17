5767735a9095015feffd7decf3ba696a5c5a858d
Victor Blomqvist <vb@viblo.se>
2025-01-17 13:29:35 +0100

    Update debug draw for pyglet 2.1.x instead of 2.0 fix #267

--- pymunk/pyglet_util.py
+++ pymunk/pyglet_util.py
@@ -125,13 +125,22 @@ class DrawOptions(pymunk.SpaceDebugDrawOptions):
         cc = pos + Vec2d.from_polar(radius, angle)
         color = outline_color.as_int()
         l = pyglet.shapes.Line(
-            pos.x, pos.y, cc.x, cc.y, width=1, color=color, batch=self.batch, group=fg
+            pos.x,
+            pos.y,
+            cc.x,
+            cc.y,
+            thickness=1,
+            color=color,
+            batch=self.batch,
+            group=fg,
         )
         self.draw_shapes.append(l)
 
     def draw_segment(self, a: Vec2d, b: Vec2d, color: SpaceDebugColor) -> None:
         c = color.as_int()
-        l = pyglet.shapes.Line(a.x, a.y, b.x, b.y, width=1, color=c, batch=self.batch)
+        l = pyglet.shapes.Line(
+            a.x, a.y, b.x, b.y, thickness=1, color=c, batch=self.batch
+        )
         self.draw_shapes.append(l)
 
     def draw_fat_segment(
